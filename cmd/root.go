7c8258394a17eff3a0b185445e6f86fbe23c8dfa
weak_ptr <weak_ptr@outlook.com>
2022-12-27 17:52:06 +0800

    refactor: 移除不必要的 capture 子命令

--- cmd/root.go
+++ cmd/root.go
@@ -18,8 +18,15 @@ package cmd
 
 import (
 	"fmt"
+	"log"
+	"net"
 	"os"
+	"strings"
+	"time"
 
+	"github.com/google/gopacket"
+	"github.com/google/gopacket/layers"
+	"github.com/google/gopacket/pcap"
 	"github.com/nnnewb/nf/internal/constants"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
@@ -29,13 +36,87 @@ var cfgFile string
 
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
-	Use:     "nf",
-	Short:   "A libpcap based traffic capture tool.",
-	Long:    `A libpcap based traffic capture tool.`,
+	Use:   "nf",
+	Short: "A libpcap based traffic capture tool.",
+	Long: "A libpcap based traffic capture tool.\n\n" +
+		"Example:\n" +
+		"    nf -i any -- udp and dst port 1701\n",
 	Version: fmt.Sprintf("%s BUILD: %s %s %s", constants.VERSION, constants.BUILD_COMMIT, constants.BUILD_TIME, constants.BUILD_LINKAGE),
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
-	Run: func(cmd *cobra.Command, args []string) {},
+	Run: func(cmd *cobra.Command, args []string) {
+		err := cmd.ParseFlags(os.Args)
+		if err != nil {
+			panic(err)
+		}
+
+		log.SetFlags(log.Lmicroseconds | log.LstdFlags)
+		i, err := cmd.PersistentFlags().GetString("interface")
+		if err != nil {
+			panic(err)
+		}
+
+		log.Printf("start capture packets on NIC %s\n", i)
+		handle, err := pcap.OpenLive(i, 0, false, time.Second)
+		if err != nil {
+			panic(err)
+		}
+		defer handle.Close()
+
+		log.Printf("used BPF: %s\n", strings.Join(args, " "))
+		err = handle.SetBPFFilter(strings.Join(args, " "))
+		if err != nil {
+			panic(err)
+		}
+
+		pktSrc := gopacket.NewPacketSource(handle, handle.LinkType())
+		for pkt := range pktSrc.Packets() {
+			var (
+				proto       string
+				src         net.IP
+				dst         net.IP
+				spt         int
+				dpt         int
+				description string
+			)
+
+			if ipLayer := pkt.Layer(layers.LayerTypeIPv4); ipLayer != nil {
+				ip := ipLayer.(*layers.IPv4)
+				src = ip.SrcIP
+				dst = ip.DstIP
+			}
+
+			if icmp4Layer := pkt.Layer(layers.LayerTypeICMPv4); icmp4Layer != nil {
+				icmp4 := icmp4Layer.(*layers.ICMPv4)
+				proto = "icmp4"
+				description = fmt.Sprintf("ICMP %s, id %d, seq %d", icmp4.TypeCode, icmp4.Id, icmp4.Seq)
+			}
+
+			if tcpLayer := pkt.Layer(layers.LayerTypeTCP); tcpLayer != nil {
+				tcp := tcpLayer.(*layers.TCP)
+				proto = "tcp"
+				spt = int(tcp.SrcPort)
+				dpt = int(tcp.DstPort)
+			}
+
+			if udpLayer := pkt.Layer(layers.LayerTypeUDP); udpLayer != nil {
+				udp := udpLayer.(*layers.UDP)
+				proto = "udp"
+				spt = int(udp.SrcPort)
+				dpt = int(udp.DstPort)
+			}
+
+			switch proto {
+			case "tcp", "udp":
+				log.Printf("%s %s:%d > %s:%d %s", proto, src, spt, dst, dpt, description)
+			case "icmp4", "icmp6":
+				log.Printf("%s %s > %s %s", proto, src, dst, description)
+			default:
+				log.Printf("unknown pkt: %s", pkt.String())
+			}
+		}
+
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -54,7 +135,7 @@ func init() {
 	// Cobra supports persistent flags, which, if defined here,
 	// will be global for your application.
 
-	rootCmd.PersistentFlags().BoolP("version", "v", false, "show version info")
+	rootCmd.PersistentFlags().StringP("nic", "i", "any", "network interface card")
 	rootCmd.PersistentFlags().StringVar(&cfgFile, "config", "", "config file (default is $HOME/.nf.yaml)")
 
 	// Cobra also supports local flags, which will only run
